cf459702909381a28751a6fdfb952e76a7850b71
Mahmodul Hasan <nicesanto92@gmail.com>
2023-05-24 13:38:15 +0600

    Creating a MERN Recipe app

---
+++ server/src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// connecting mongoose
+mongoose
+  .connect(process.env.MONGO_SERVER)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err));
+//server
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
+});
